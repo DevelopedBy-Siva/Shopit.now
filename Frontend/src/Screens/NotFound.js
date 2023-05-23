40d5e50cc750b49b14a3dc8d414d7ea3a3dc0cdb
DevelopedBy-Siva <developedby.siva@gmail.com>
2023-05-24 00:18:48 +0530

    Restyle the not-found container

--- Frontend/src/Screens/NotFound.js
+++ Frontend/src/Screens/NotFound.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { TfiFaceSad } from "react-icons/tfi";
 import { Link } from "react-router-dom";
 import "../css/not-found.css";
 
@@ -9,16 +8,12 @@ class NotFound extends Component {
     return (
       <>
         <div className="not-found-container">
-          <h1>
-            <TfiFaceSad className="not-found-icon" />
-          </h1>
+          <h1>404</h1>
           <h2>Page Not Found</h2>
           <p>Uh oh, we can't seem to find the page you're looking for.</p>
-          <h3>
-            <Link className="go-home" to="/">
-              Go Home
-            </Link>
-          </h3>
+          <Link className="go-home" to="/">
+            Go Home
+          </Link>
         </div>
       </>
     );
