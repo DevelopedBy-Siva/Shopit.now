0cd8bb8a5a9b800d1c1cf2ce1bdb21436b8c494b
DevelopedBy-Siva <developedby.siva@gmail.com>
2023-07-09 19:08:39 +0530

    Create a custom hook to scroll between carousel slides

--- Frontend/src/Components/Home/BestSellers.js
+++ Frontend/src/Components/Home/BestSellers.js
@@ -1,87 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { api_endpoints as API_ENDPOINT, formUrl as URL } from "../../api/api";
 import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
 import SkeletonLoader from "../Loader/SkeletonLoader";
-import CarouselNavigate from "./CarouselNavigate";
+import useCarouselNavigate from "../Hooks/useCarouselNavigation";
 import "../../css/bestseller.css";
 
-class BestSellers extends CarouselNavigate {
-  state = {
+function BestSellers() {
+  const [api, setApi] = useState({
     loading: true,
     data: [],
     error: false,
-  };
-  componentDidMount() {
-    this.getProducts();
-  }
+  });
 
-  getProducts = async () => {
-    this.setState({ loading: true, error: false });
-    await axios
+  const { scrollRef, itemRef, btnActive, handleScrollLeft, handleScrollRight } =
+    useCarouselNavigate(api.data);
+
+  useEffect(() => {
+    setApi({ data: [], loading: true, error: false });
+    axios
       .get(`${URL(API_ENDPOINT.productApi)}/view/bestsellers`)
       .then(({ data }) => {
-        this.setState({ data, loading: false });
+        setApi({ data, loading: false });
       })
       .catch(() => {
-        this.setState({ loading: false, error: true });
+        setApi({ data: [], loading: false, error: true });
       });
-  };
+  }, []);
 
-  render() {
-    const { data, error, loading } = this.state;
-    const { onClick } = this.props;
-    return (
-      <div className="bestseller-main-container">
-        <div className="bestseller-main-header">
-          <h2>Best sellers in Mobiles, Laptops & Accessories</h2>
-          {!loading && !error && data.length > 0 && (
-            <div className="home-corousel-btns">
-              <button onClick={this.handleScrollLeft}>
-                <BiChevronLeft />
-              </button>
-              <button onClick={this.handleScrollRight}>
-                <BiChevronRight />
-              </button>
-            </div>
-          )}
-        </div>
-
-        <div className="bestseller-main-sub-container">
-          {loading ? (
-            <SkeletonLoader />
-          ) : error ? (
-            <span className="bestseller-error">
-              Couldn't load the products.
-            </span>
-          ) : (
-            <>
-              <div ref={this.scrollRef} className="bestseller-container">
-                {data.map((item) => {
-                  const { id, title, thumbnail, price } = item;
-                  const imgUrl = `data:${thumbnail.type};base64,${thumbnail.picByte}`;
-                  return (
-                    <div
-                      onClick={() => onClick(item)}
-                      key={id}
-                      className="best-seller-item-container"
-                      ref={this.itemRef}
-                    >
-                      <img src={imgUrl} alt="bestsellers" />
-                      <div className="bestseller-details">
-                        <h4>{title}</h4>
-                        <h5>${price}</h5>
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-            </>
-          )}
-        </div>
+  const { data, error, loading } = api;
+  return (
+    <div className="bestseller-main-container">
+      <div className="bestseller-main-header">
+        <h2>Best sellers in Mobiles, Laptops & Accessories</h2>
+        {!loading && !error && data.length > 0 && (
+          <div className="home-corousel-btns">
+            <button onClick={handleScrollLeft} disabled={btnActive.left}>
+              <BiChevronLeft />
+            </button>
+            <button onClick={handleScrollRight} disabled={btnActive.right}>
+              <BiChevronRight />
+            </button>
+          </div>
+        )}
       </div>
-    );
-  }
+
+      <div className="bestseller-main-sub-container">
+        {loading ? (
+          <SkeletonLoader />
+        ) : error ? (
+          <span className="bestseller-error">Couldn't load the products.</span>
+        ) : (
+          <div className="bestseller-container" ref={scrollRef}>
+            {data.map((item) => {
+              const { id, title, thumbnail, price } = item;
+              const imgUrl = `data:${thumbnail.type};base64,${thumbnail.picByte}`;
+              return (
+                <div
+                  ref={itemRef}
+                  key={id}
+                  className="best-seller-item-container"
+                >
+                  <img src={imgUrl} alt="bestsellers" />
+                  <div className="bestseller-details">
+                    <h4>{title}</h4>
+                    <h5>${price}</h5>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default BestSellers;
--- Frontend/src/Components/Home/TodaysDeals.js
+++ Frontend/src/Components/Home/TodaysDeals.js
@@ -1,83 +1,77 @@
+import { useEffect, useState } from "react";
 import { api_endpoints as API_ENDPOINT, formUrl as URL } from "../../api/api";
 import axios from "axios";
 import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
 import SkeletonLoader from "../Loader/SkeletonLoader";
-import CarouselNavigate from "./CarouselNavigate";
+import useCarouselNavigate from "../Hooks/useCarouselNavigation";
 import "../../css/todaysdeal.css";
 
-class TodaysDeals extends CarouselNavigate {
-  state = {
-    data: [],
+function TodaysDeals() {
+  const [api, setApi] = useState({
     loading: true,
+    data: [],
     error: false,
-  };
-  async componentDidMount() {
-    this.setState({ loading: true, error: false });
-    await axios
+  });
+
+  const { scrollRef, itemRef, btnActive, handleScrollLeft, handleScrollRight } =
+    useCarouselNavigate(api.data);
+
+  useEffect(() => {
+    setApi({ data: [], loading: true, error: false });
+    axios
       .get(`${URL(API_ENDPOINT.productApi)}/view/new-deals`)
       .then(({ data }) => {
-        this.setState({ data, loading: false });
+        setApi({ data, loading: false });
       })
       .catch(() => {
-        this.setState({ loading: false, error: true });
+        setApi({ data: [], loading: false, error: true });
       });
-  }
+  }, []);
 
-  render() {
-    const { data, loading, error } = this.state;
-    const { onClick } = this.props;
-
-    return (
-      <div className="todays-deal-container">
-        <div className="todays-deal-header">
-          <h2>Today's Deals</h2>
-          {!loading && !error && data.length > 0 && (
-            <div className="home-corousel-btns">
-              <button onClick={this.handleScrollLeft}>
-                <BiChevronLeft />
-              </button>
-              <button onClick={this.handleScrollRight}>
-                <BiChevronRight />
-              </button>
-            </div>
-          )}
-        </div>
-        <div className="todays-deal-sub-container">
-          {loading ? (
-            <SkeletonLoader />
-          ) : error ? (
-            <span className="todays-deal-error">
-              Couldn't load today's deals
-            </span>
-          ) : (
-            <div ref={this.scrollRef} className="todaysdeal-container">
-              {data.map((item) => {
-                const { id, title, thumbnail, price } = item;
-                const fraudPrice = price * 2;
-                const imgUrl = `data:${thumbnail.type};base64,${thumbnail.picByte}`;
-                return (
-                  <div
-                    onClick={() => onClick(item)}
-                    className="item-container"
-                    ref={this.itemRef}
-                    key={id}
-                  >
-                    <img src={imgUrl} alt="todays-deals" />
-                    <div className="todaysdeals-details">
-                      <h4>{title}</h4>
-                      <h5>${price}</h5>
-                      <h6>${fraudPrice}</h6>
-                    </div>
-                    <span className="todaysdeal-discount">-50%</span>
-                  </div>
-                );
-              })}
-            </div>
-          )}
-        </div>
+  const { data, loading, error } = api;
+  return (
+    <div className="todays-deal-container">
+      <div className="todays-deal-header">
+        <h2>Today's Deals</h2>
+        {!loading && !error && data.length > 0 && (
+          <div className="home-corousel-btns">
+            <button onClick={handleScrollLeft} disabled={btnActive.left}>
+              <BiChevronLeft />
+            </button>
+            <button onClick={handleScrollRight} disabled={btnActive.right}>
+              <BiChevronRight />
+            </button>
+          </div>
+        )}
       </div>
-    );
-  }
+      <div className="todays-deal-sub-container">
+        {loading ? (
+          <SkeletonLoader />
+        ) : error ? (
+          <span className="todays-deal-error">Couldn't load today's deals</span>
+        ) : (
+          <div ref={scrollRef} className="todaysdeal-container">
+            {data.map((item) => {
+              const { id, title, thumbnail, price } = item;
+              const fraudPrice = price * 2;
+              const imgUrl = `data:${thumbnail.type};base64,${thumbnail.picByte}`;
+              return (
+                <div className="item-container" ref={itemRef} key={id}>
+                  <img src={imgUrl} alt="todays-deals" />
+                  <div className="todaysdeals-details">
+                    <h4>{title}</h4>
+                    <h5>${price}</h5>
+                    <h6>${fraudPrice}</h6>
+                  </div>
+                  <span className="todaysdeal-discount">-50%</span>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default TodaysDeals;
