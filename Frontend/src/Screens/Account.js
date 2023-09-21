1c916d1dffb7b52a74c7d7d12ca3c6f9ff7d8c1b
DevelopedBy-Siva <developedby.siva@gmail.com>
2023-09-21 12:26:44 +0530

    Set as default address on click

--- Frontend/src/Screens/Account.js
+++ Frontend/src/Screens/Account.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaTruck } from "react-icons/fa";
 import { MdRemoveCircle, MdLocationOn, MdKey, MdClose } from "react-icons/md";
+import { HiOutlinePlusSm, HiOutlineMinusSm } from "react-icons/hi";
 import ReactFocusLock from "react-focus-lock";
 import { useHistory } from "react-router-dom";
 import * as Yup from "yup";
@@ -70,6 +71,23 @@ const validatePaasword = Yup.object().shape({
     .oneOf([Yup.ref("newPswd"), null], "Passwords doesn't match")
     .label("Confirm Password")
     .trim(),
+});
+
+const validateAddress = Yup.object().shape({
+  houseNo: Yup.string()
+    .required("Enter the house no/street")
+    .min(3)
+    .max(20)
+    .label("House No")
+    .trim(),
+  town: Yup.string().required("Enter the town").max(25).label("Town").trim(),
+  city: Yup.string().required("Enter the city").label("City").trim(),
+  state: Yup.string().max(20).required("Enter the State").label("State").trim(),
+  pincode: Yup.string()
+    .min(5, "Invalid pincode")
+    .max(10, "Invalid pincode")
+    .required("Enter the pincode")
+    .label("Pincode"),
 });
 
 export default function Account() {
@@ -353,13 +371,294 @@ function DeleteAccount({ toggle, loading, setLoading }) {
   );
 }
 
+const addressValues = {
+  houseNo: "",
+  town: "",
+  city: "",
+  state: "",
+  pincode: "",
+};
+
 function AddAddress({ toggle, loading, setLoading }) {
+  const [api, setApi] = useState({
+    loading: true,
+    data: [],
+    error: null,
+  });
+  const [todo, setTodo] = useState({
+    type: null,
+    values: addressValues,
+  });
+
+  async function handleAddress() {
+    setLoading(true);
+  }
+
+  useEffect(() => {
+    async function getApi() {
+      const user = getCurrentUser();
+      await axios
+        .get(`${URL(api_endpoints.userOperations)}/address/${user.id}`, {
+          headers: {
+            Authorization: getJwt(),
+          },
+        })
+        .then(({ data }) => {
+          setApi({ loading: false, error: null, data });
+        })
+        .catch(() => {
+          setApi({ data: [], loading: false, error: "error" });
+        });
+    }
+    getApi();
+  }, []);
+
+  const handleTask = (type, values = addressValues) => {
+    setTodo({ type, values });
+  };
+
+  const goBack = () => setTodo({ type: null, values: addressValues });
+
+  const btnInactive = (e) => e.target.classList.add("account-address-inactive");
+  const btnRemoveInactive = (e) =>
+    e.target.classList.remove("account-address-inactive");
+
+  const handleDefault = async (e, addressId) => {
+    const user = getCurrentUser();
+    setLoading(true);
+    btnInactive(e);
+    await axios
+      .put(
+        `${URL(api_endpoints.userOperations)}/update-default-address/${
+          user.id
+        }/${addressId}`,
+        null,
+        {
+          headers: { Authorization: getJwt() },
+        }
+      )
+      .then(() => {
+        const data = [...api.data];
+        data.forEach((item) => {
+          if (item.id === addressId) item.defaultAddress = true;
+          else item.defaultAddress = false;
+        });
+        setApi({ ...api, data });
+      })
+      .catch(() => {
+        toast.error("Something went wrong");
+      });
+    setLoading(false);
+    btnRemoveInactive(e);
+  };
+
   return (
     <FocusWrapper toggle={toggle}>
       <div className="account-add-address">
         <ModalHeadingWrapper loading={loading} toggle={toggle}>
           Your Address
         </ModalHeadingWrapper>
+        {todo.type === null ? (
+          <div className="account-user-address account-address-wrapper">
+            <div className="add-new-address-button-container">
+              <HiOutlinePlusSm />
+              <button disabled={loading} onClick={() => handleTask("ADD")}>
+                Add new address
+              </button>
+            </div>
+            <div className="address-sub-heading">
+              <HiOutlineMinusSm />
+              <span>Saved addresses</span>
+            </div>
+            <div className="account-saved-address-content">
+              {api.loading ? (
+                <p>Fetching saved addresses. Please wait...</p>
+              ) : api.error ? (
+                <p>Something went wrong. Failed to get saved addresses</p>
+              ) : api.data.length === 0 ? (
+                <p>No saved addresses found</p>
+              ) : (
+                <div className="address-box-container">
+                  {api.data.map((item, index) => {
+                    const { city, houseNo, pincode, state, town } =
+                      item.addressDetails;
+                    return (
+                      <div className="address-box" key={index}>
+                        <span>{`${houseNo}, ${town}, ${city}, ${state}, ${pincode}`}</span>
+                        <div className="address-btn-container">
+                          <button
+                            disabled={loading}
+                            onClick={() =>
+                              handleTask("EDIT", item.addressDetails)
+                            }
+                          >
+                            Edit
+                          </button>
+
+                          <span>|</span>
+                          <button disabled={loading}>Remove</button>
+                          {!item.defaultAddress && (
+                            <>
+                              <span>|</span>
+                              <button
+                                disabled={loading}
+                                onClick={(e) => handleDefault(e, item.id)}
+                              >
+                                Set as default
+                              </button>
+                            </>
+                          )}
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
+            </div>
+          </div>
+        ) : (
+          <div className="account-address-content account-address-wrapper">
+            <button
+              disabled={loading}
+              onClick={goBack}
+              className="account-address-go-back"
+            >
+              Back
+            </button>
+            <div className="address-sub-heading">
+              <HiOutlinePlusSm />
+              <span>
+                {todo.type === "EDIT" ? "Edit address" : "Add new address"}
+              </span>
+            </div>
+            <Formik
+              initialValues={{ ...todo.values }}
+              onSubmit={handleAddress}
+              validationSchema={validateAddress}
+            >
+              {({
+                handleSubmit,
+                handleChange,
+                errors,
+                setFieldTouched,
+                touched,
+                isValid,
+                dirty,
+                values,
+              }) => {
+                return (
+                  <form
+                    onSubmit={handleSubmit}
+                    className="account-change-password-content-form"
+                  >
+                    <div>
+                      <label name="houseNo">House No/Street:</label>
+                      <input
+                        name="houseNo"
+                        onChange={handleChange("houseNo")}
+                        disabled={loading}
+                        maxLength={10}
+                        type="text"
+                        value={values.houseNo}
+                        onBlur={() => setFieldTouched("houseNo")}
+                      />
+                      {errors.houseNo && touched.houseNo && (
+                        <span className="account-change-password-input-error">
+                          {errors.houseNo}
+                        </span>
+                      )}
+                    </div>
+                    <div>
+                      <label name="town">Town:</label>
+                      <input
+                        name="town"
+                        onChange={handleChange("town")}
+                        disabled={loading}
+                        maxLength={20}
+                        type="text"
+                        value={values.town}
+                        onBlur={() => setFieldTouched("town")}
+                      />
+                      {errors.town && touched.town && (
+                        <span className="account-change-password-input-error">
+                          {errors.town}
+                        </span>
+                      )}
+                    </div>
+                    <div>
+                      <label name="city">City:</label>
+                      <input
+                        name="city"
+                        onChange={handleChange("city")}
+                        disabled={loading}
+                        maxLength={20}
+                        type="text"
+                        value={values.city}
+                        onBlur={() => setFieldTouched("city")}
+                      />
+                      {errors.city && touched.city && (
+                        <span className="account-change-password-input-error">
+                          {errors.city}
+                        </span>
+                      )}
+                    </div>
+                    <div>
+                      <label name="state">State:</label>
+                      <input
+                        name="state"
+                        onChange={handleChange("state")}
+                        disabled={loading}
+                        maxLength={20}
+                        type="text"
+                        value={values.state}
+                        onBlur={() => setFieldTouched("state")}
+                      />
+                      {errors.state && touched.state && (
+                        <span className="account-change-password-input-error">
+                          {errors.state}
+                        </span>
+                      )}
+                    </div>
+                    <div>
+                      <label name="pincode">Pincode:</label>
+                      <input
+                        name="pincode"
+                        onChange={handleChange("pincode")}
+                        disabled={loading}
+                        maxLength={10}
+                        type="text"
+                        value={values.pincode}
+                        onBlur={() => setFieldTouched("pincode")}
+                      />
+                      {errors.pincode && touched.pincode && (
+                        <span className="account-change-password-input-error">
+                          {errors.pincode}
+                        </span>
+                      )}
+                    </div>
+                    <button
+                      type="submit"
+                      disabled={loading}
+                      className={
+                        !isValid || !dirty
+                          ? "account-change-password-btn-grey"
+                          : ""
+                      }
+                    >
+                      {loading ? (
+                        <Loader style={{ width: "18px", height: "18px" }} />
+                      ) : todo.type === "EDIT" ? (
+                        "Edit Address"
+                      ) : (
+                        "Add Address"
+                      )}
+                    </button>
+                  </form>
+                );
+              }}
+            </Formik>
+          </div>
+        )}
       </div>
     </FocusWrapper>
   );
